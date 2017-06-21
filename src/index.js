4b22c6e36f9da5290b18a384644a635e66c159d6
Otávio Augusto Soares <otaviokk@gmail.com>
2017-06-21 13:17:02 -0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept(); 
+}
+
 registerServiceWorker();
